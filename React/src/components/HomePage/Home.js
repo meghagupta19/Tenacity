ed32c15c12d5d74bfc38ea5c9429f52510034ab0
megha <meghagupta190801@gmail.com>
2021-01-29 14:37:35 +0530

    Update Home.js

--- React/src/components/HomePage/Home.js
+++ React/src/components/HomePage/Home.js
@@ -1,44 +1,34 @@
 import React from "react";
-import { Navbar, Nav, Container, Row, Col} from "react-bootstrap";
+
 import "./Home.css";
+import Header from './Header'
+import Footer from './Footer'
+import Images from './Images'
+import Quotes from './Quotes'
+import DonateNow from './DonateNow'
+import VolunteerNow from './VolunteerNow'
+import MiddleContainer from './MiddleContainer'
+
 
 function Home() {
   return (
     <div>
-      <div className="top">
-        <Navbar collapseOnSelect expand="lg" bg="transparent" variant="dark" fixed="top">
-          <Navbar.Brand href="#home" classNam="name">Prayaas</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" className="toggle-bar"/>
-          <Navbar.Collapse id="responsive-navbar-nav" className="navbg">
-            <Nav className="ml-auto">
-              <Nav.Link href="#home" className="list"> Home </Nav.Link>
-              <Nav.Link href="#donate" className="list"> Donate Now </Nav.Link>
-              <Nav.Link href="#impact" className="list"> Impact</Nav.Link>
-              <Nav.Link href="#causes" className="list"> Causes </Nav.Link>
-              <Nav.Link href="#contact" className="list"> Contact </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <div className="shade"></div>
+      
+      <div> <Header/> </div>     
 
-        <div className="context col col-sm-12 col-md-12 col-lg-5">
-          <h1 className="heading1">Prayaas- Ek Koshish</h1>
-          <p>
-            At the end of the day it’s not about what you have or even what
-            you’ve accomplished… it’s about who you’ve lifted up, who you’ve
-            made better. It’s about what you’ve given back.
-          </p>
-        </div>
+      <div className="section">
+        <Quotes/>
+        <Images/>
+        <MiddleContainer/>
+        <DonateNow/>
+        <VolunteerNow/>
       </div>
 
       <div className="footer">
-        <Container>
-          <Row>
-            <Col></Col>
-          </Row>
-        </Container>
+        <Footer/>
       </div>
     </div>
+    
   );
 }
 
